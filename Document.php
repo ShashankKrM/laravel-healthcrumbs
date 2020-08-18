c41e4be5e3b2946d6097ffadbb32bf968f57eb3d
Shashank Kumar <35068625+ShashankKrM@users.noreply.github.com>
2020-08-18 19:12:06 +0530

    Add files via upload

---
+++ Document.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Document extends Model
+{
+//    protected $fillable = ['id','name','user_id','property_id','room_id','comments','path','url','type','status'];
+    
+//    public function user(){
+//        return $this->belongsTo('App\User');
+//    }
+}
---
+++ User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'email' ,'addedby', 'password', 'role', 'phone', 'addressl1' , 'city', 'state', 'country', 'zipcode','status','confirmation_code','confirmation_otp','confirmed','image','facebook','linkedin','googleplus','twitter', 'api_token','myself','father','daughter','spouse','mother','son'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
